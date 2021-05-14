8a13105e975b317703535435f29f8158f9fe124b
aswitha <ashwitha447@gmail.com>
2021-05-14 17:23:33 +0530

    second commit as tester

--- src/javatime1/test2.java
+++ src/javatime1/test2.java
@@ -5,6 +5,9 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("hello git"+"okay done");
+        
+        
+        System.out.println("try to get branch and pull it");
 	}
 
 }
