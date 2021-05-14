61700344377395120694f083a9bb3fe8c215654d
aswitha <ashwitha447@gmail.com>
2021-05-14 17:25:56 +0530

    i am doing my first commit as ashjimin to branchx

--- src/javatime1/test2.java
+++ src/javatime1/test2.java
@@ -6,7 +6,7 @@ public class test2 {
 		// TODO Auto-generated method stub
         System.out.println("hello git"+"okay done");
         
-        
+        System.out.println("try to get branch and pull it");
         System.out.println("try to get branch and pull it");
 	}
 
